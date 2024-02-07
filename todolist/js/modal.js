174d4561194ff8c1fb8849acc871f4978decb814
code1218 <skjil1218@gmail.com>
2024-02-07 13:55:26 +0900

    feat: todo 추가, todo 불러오기, todo 수정하기(진행중)

--- todolist/js/modal.js
+++ todolist/js/modal.js
@@ -1,3 +1,44 @@
+// window.onload = () => {
+//     // const day = ["일", "월", "화", "수", "목", "금", "토"];
+
+//     // let now = new Date();
+//     // console.log(now.getFullYear());
+//     // console.log(now.getMonth() + 1);
+//     // console.log(now.getDate());
+//     // console.log(day[now.getDay()]);
+
+//     let arr = new Array();
+
+//     let obj = {
+//         id: 1,
+//         name: "김준일"
+//     }
+
+//     let obj2 = {
+//         id: 2,
+//         name: "김준이"
+//     }
+
+//     arr.push(obj);
+//     arr.push(obj2);
+
+//     console.log(arr);
+
+//     console.log(JSON.stringify(arr));
+//     console.log(JSON.stringify(obj));
+//     console.log(JSON.stringify(obj2));
+
+//     let jsonArr = JSON.stringify(arr);
+
+//     console.log(JSON.parse(jsonArr));
+
+// }
+
+window.onkeyup = (e) => {
+    if(e.keyCode === 27) {
+        handleCancelClick();
+    }
+}
 
 function handleAddTodoModalOpen() {
     const modal = document.querySelector(".root-modal");
@@ -11,7 +52,56 @@ function handleAddTodoModalOpen() {
     modal.classList.add("modal-show");
 }
 
+function handleEditTodoModalOpen(todoId) {
+    const modal = document.querySelector(".root-modal");
+    const title = modal.querySelector(".modal-title");
+    const todoInput = modal.querySelector(".todo-input");
+    const submitButton = modal.querySelector(".modal-button");
+    title.innerHTML = "수정하기";
+
+    let todoListJson = localStorage.getItem("todoList");
+    let todoList = todoListJson !== null ? JSON.parse(todoListJson) : new Array();
+
+    let findTodoByTodoId = todoList.filter(todo => todo.todoId === todoId)[0];
+
+    todoInput.value = findTodoByTodoId.content;
+    submitButton.onclick = handleEditTodoSubmit;
+    
+    modal.classList.add("modal-show");
+}
+
+function convertDateKor(curruntDate) {
+    const dayKors = ["일", "월", "화", "수", "목", "금", "토"];
+    const year = curruntDate.getFullYear();
+    const month = curruntDate.getMonth() + 1;
+    const date = curruntDate.getDate();
+    const day = dayKors[curruntDate.getDay()];
+    return `${year}년 ${month}월 ${date}일(${day})`;
+}
+
 function handleAddTodoSubmit() {
+    const modal = document.querySelector(".root-modal");
+    const todoInput = modal.querySelector(".todo-input");
+    modal.classList.remove("modal-show");
+
+    let todoListJson = localStorage.getItem("todoList");
+    let todoList = todoListJson !== null ? JSON.parse(todoListJson) : new Array();
+
+    let lastTodoId = todoList.length === 0 ? 0 : todoList[todoList.length - 1].todoId;
+
+    let todoObject = {
+        todoId: lastTodoId + 1,
+        content: todoInput.value,
+        date: convertDateKor(new Date())
+    }
+
+    todoList.push(todoObject);
+
+    localStorage.setItem("todoList", JSON.stringify(todoList));
+    getTodoList();
+}
+
+function handleEditTodoSubmit() {
     const modal = document.querySelector(".root-modal");
     modal.classList.remove("modal-show");
 }
