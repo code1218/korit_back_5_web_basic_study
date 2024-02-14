95a5994de9b7ee114b266b96e37c52941aa03c6b
code1218 <skjil1218@gmail.com>
2024-02-14 14:00:15 +0900

    feat: async_await

--- array_study/js/data.js
+++ array_study/js/data.js
@@ -20,6 +20,19 @@
 // console.log(obj);
 // console.log(obj2);
 
+// 비구조할당
+
+// let obj = {
+//     id: 1,
+//     name: "김준일",
+//     age:31
+// }
+
+// let { id, age } = obj;
+
+// console.log(age);
+
+// fetch("http://localhost:8080/data_array/data/addition");
 
 let dataList = [];
 
@@ -32,6 +45,8 @@ window.onload = () => {
         if(e.keyCode === 13) {
             const inputValue = addInput.value;
 
+            [ {id: 1, content: "test1"}, {id: 2, content: "test2"}, {id: 3, content: "test2"} ]
+
             const lastId = dataList.length === 0 ? 0 : dataList[dataList.length - 1].id;
 
             // [ {id: 1,content: inputValue}, {id: 2,content: inputValue}, {id: 3,content: inputValue} ]
@@ -41,13 +56,35 @@ window.onload = () => {
                 content: inputValue
             }
 
+            fetch("http://localhost:8080/data_array/data/addition", {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify(dataObj)
+            }).then(response => {
+                console.log(response);
+            })
+
             dataList = [ ...dataList, dataObj ];
-            
+
             addInput.value = "";
 
             getDataList();
         }
     }
+}
+
+function ContentData({ id, content }) {
+    return `
+        <li>
+            <span>${id}번 </span> 
+            <span>${content}</span>
+            <input type="text" class="edit-inputs" value="${content}">
+            <button onclick="editData(${id})">수정</button>
+            <button onclick="removeData(${id})">삭제</button>
+        </li>
+    `;
 }
 
 function getDataList() {
@@ -56,14 +93,37 @@ function getDataList() {
     contentList.innerHTML = "";
 
     for(let dataObj of dataList) {
-        contentList.innerHTML += `
-            <li>
-                <span>${dataObj.id}번 </span> 
-                <span>${dataObj.content}</span>
-                <input type="text" class="edit-inputs" value="${dataObj.content}">
-                <button>수정</button>
-                <button>삭제</button>
-            </li>
-        `;
+        contentList.innerHTML += ContentData(dataObj);
     }
+}
+
+function removeData(id) {
+    // [ {id: 1, content: "test1"}, {id: 2, content: "test2"}, {id: 3, content: "test2"} ]
+
+    dataList = dataList.filter((dataObj) => dataObj.id !== id);
+
+    // [ {id: 1, content: "test1"}, {id: 3, content: "test2"} ]
+
+    getDataList();
+}
+
+function editData(id) {
+    let findIndex = -1;
+
+    for(let i = 0; i < dataList.length; i++) {
+        if(dataList[i].id === id) {
+            findIndex = i;
+            break;
+        }
+    }
+
+    let findObj = dataList.filter((dataObj) => dataObj.id === id)[0];
+
+    findIndex = dataList.indexOf(findObj);
+
+    const editInputs = document.querySelectorAll(".edit-inputs");
+
+    dataList[findIndex].content = editInputs[findIndex].value;
+
+    getDataList();
 }
---
+++ insert_and_select/js/insert.js
@@ -0,0 +1,54 @@
+async function handleSubmitClick() {
+    const dataInputs = document.querySelectorAll(".data-inputs");
+
+    const data = {
+        name: dataInputs[0].value,
+        age: dataInputs[1].value
+    };
+
+    console.log(data);
+    const jsonData = JSON.stringify(data);
+    console.log(jsonData);
+
+    const option = {
+        method: "post",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: jsonData
+    };
+
+    // fetch("http://localhost:8080/insert_and_select/data/addition", option)
+    // .then((response) => {
+    //     response.json()
+    //     .then((json) => {
+    //         console.log(json);
+    //         console.log("test");
+    //     })
+    // }).catch((error) => {
+    //     console.log("프로미스 예외처리");
+    //     console.log(error);
+    // });
+
+    try {
+        const response = await fetch("http://localhost:8080/insert_and_select/data/addition", option);
+    
+        if(!response.ok) {
+            throw await response.json();
+        }
+
+        console.log(response);
+    
+        const json = await response.json();
+    
+        console.log(json);
+    
+        console.log("test");
+
+    } catch(error) {
+        console.log(error);
+        alert(error.errorMessage);
+    }
+
+
+}
