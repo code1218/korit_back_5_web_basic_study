34af6deacdac7d3196d3c1a739f4429e109ef82b
code1218 <skjil1218@gmail.com>
2024-02-27 17:53:47 +0900

    promise

---
+++ js/promise4.js
@@ -0,0 +1,37 @@
+main();
+
+function main() {
+    new Promise((resolve, reject) => {
+        let result = [];
+        setTimeout(() => {
+            resolve([result, 1]);
+        }, 3000);
+    })
+    
+    .then(([ result, num ]) => {
+        result.push(num);
+        new Promise((resolve, reject) => {
+            setTimeout(() => {
+                resolve([result, 2]);
+            }, 2000);
+        })
+        
+        .then(([ result, num ]) => {
+            result.push(num);
+            new Promise((resolve, reject) => {
+                setTimeout(() => {
+                    resolve([result, 3]);
+                }, 1000);
+            })
+            
+            .then(([ result, num ]) => {
+                result.push(num);
+                return result;
+            })
+            
+            .then(r => console.log(r));
+        });
+    });
+
+
+}
